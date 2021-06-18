7b820d6c1396cdfb4ac9884b45adedd410b3a4d4
sm0l voicc <72088954+sm0lvoicc@users.noreply.github.com>
2021-06-18 12:28:38 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,12 +1,13 @@
-const { MessageEmbed } = require('discord.js');
-const Timeout = new Set();
-const ms = require('ms');
+const { MessageEmbed } = require('discord.js'); //calling the discord.js module
+const Timeout = new Set(); //a set is basically a collection or a vactor list if you know java
+const ms = require('ms'); //ms is ms 
+const { prefix }= require('../config.json'); //destructuring and then calling the prefix
 module.exports = async(bot, message) =>{
         /**
      *@param {Client} bot - the sweet bot of mine
      *@param {string} message - the message i gotted
      */
-     const args = message.content.slice(prefix.length).trim().split(/ +/g);
+     const args = message.content.slice(prefix.length).trim().split(/ +/g); // so this will slice the message by prefix's length, and then it will split it, / +/g is a regex which will basically make your text into an array, the elements will be divided by the whitespaces
      const cmd = args.shift().toLowerCase();
      if (cmd.length === 0) return;
      const command = bot.commands.get(cmd);
